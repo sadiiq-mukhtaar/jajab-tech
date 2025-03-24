54326f4fb0f4ee7ac1d5f69c14cd5f9af0dbadbf
sadiiq-mukhtaar <sadiiq2077@gmail.com>
2025-03-24 11:27:17 -0700

    style : the navbar with a beautiful design

--- app/NavBarLargeScreen.tsx
+++ app/NavBarLargeScreen.tsx
@@ -7,9 +7,29 @@ import { FiLogOut } from "react-icons/fi";
 import { HiUsers } from "react-icons/hi";
 import { MdDashboard } from "react-icons/md";
 import Spinner from "./components/Spinner";
-
+import { usePathname } from "next/navigation";
+import classnames from "classnames";
 const NavBar = () => {
-  const { data: ession, status } = useSession();
+  const { status } = useSession();
+  const current = usePathname();
+  const dashboardExpression = current === "/dashboard";
+  const customersExpression = current === "/customers";
+  const dashboardStyle = {
+    "bg-[#4D55CC]": dashboardExpression,
+    "font-semibold": true,
+    "px-2": dashboardExpression,
+    "py-1": dashboardExpression,
+    "rounded-sm": dashboardExpression,
+  };
+
+  const customerStyle = {
+    "bg-[#4D55CC]": customersExpression,
+    "font-semibold": true,
+    "px-2": customersExpression,
+    "py-1": customersExpression,
+    "rounded-sm": customersExpression,
+  };
+
   return (
     <>
       <Flex
@@ -22,13 +42,19 @@ const NavBar = () => {
         <Box>
           <Heading>JaJab Tech</Heading>
           <Box className="mt-15">
-            <Flex align={"center"} gap={"2"} mb={"6"}>
-              <MdDashboard />{" "}
-              <Link href={"/dashboard"} className="font-semibold">
-                Dashboard
-              </Link>
+            <Flex
+              align={"center"}
+              gap={"2"}
+              mb={"6"}
+              className={classnames(dashboardStyle)}
+            >
+              <MdDashboard /> <Link href={"/dashboard"}>Dashboard</Link>
             </Flex>
-            <Flex align={"center"} gap={"2"} className="font-semibold">
+            <Flex
+              align={"center"}
+              gap={"2"}
+              className={classnames(customerStyle)}
+            >
               <HiUsers /> <Link href={"/customers"}>Customers</Link>
             </Flex>
           </Box>
--- app/NavBarSmallScreen.tsx
+++ app/NavBarSmallScreen.tsx
@@ -1,11 +1,15 @@
+"use client";
 import { Flex, Heading } from "@radix-ui/themes";
+import { useSession } from "next-auth/react";
 import Link from "next/link";
 import React from "react";
 import { FiLogOut } from "react-icons/fi";
 import { LuUsersRound } from "react-icons/lu";
 import { MdDashboard } from "react-icons/md";
+import Spinner from "./components/Spinner";
 
 const NavBarSmallScreen = () => {
+  const { status } = useSession();
   return (
     <div className="bg-[#4D55CC] shadow-md p-4">
       <Flex justify="between" align="center">
@@ -17,9 +21,17 @@ const NavBarSmallScreen = () => {
           <Link href="/customers">
             <LuUsersRound className="text-xl" />
           </Link>
-          <Link href="/logout">
-            <FiLogOut className="text-xl" />
-          </Link>
+          {status === "authenticated" && (
+            <Link href="/api/auth/signout">
+              <FiLogOut className="text-xl" />
+            </Link>
+          )}
+          {status === "unauthenticated" && (
+            <Link href="/api/auth/signin">
+              <FiLogOut className="text-xl" />
+            </Link>
+          )}
+          {status === "loading" && <Spinner />}
         </Flex>
       </Flex>
     </div>
