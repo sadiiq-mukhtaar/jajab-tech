2883f9d7bcf916e94b011231fa49e1642abc853d
sadiiq-mukhtaar <sadiiq2077@gmail.com>
2025-03-24 09:07:35 -0700

    make : some buttons disable in unauthenticated

--- app/customers/CustomerActions.tsx
+++ app/customers/CustomerActions.tsx
@@ -4,9 +4,11 @@ import React from "react";
 import FilterByServiceNeed from "./FilterByServiceNeed";
 import FilterByStatus from "./FilterByStatus";
 import { useRouter } from "next/navigation";
+import { useSession } from "next-auth/react";
 
 const CustomerActions = () => {
   const router = useRouter();
+  const { status } = useSession();
   return (
     <Flex p={"5"} justify={"between"} wrap={"wrap"} gapY={"4"} gapX={"5"}>
       <Flex gap={"5"}>
@@ -14,12 +16,14 @@ const CustomerActions = () => {
         <FilterByStatus />
       </Flex>
       <Box>
-        <button
-          onClick={() => router.push("/customers/add")}
-          className="bg-[#4D55CC]  px-3 py-1 rounded-lg cursor-pointer"
-        >
-          Add New Customer
-        </button>
+        {status === "authenticated" && (
+          <button
+            onClick={() => router.push("/customers/add")}
+            className="bg-[#4D55CC]  px-3 py-1 rounded-lg cursor-pointer"
+          >
+            Add New Customer
+          </button>
+        )}
       </Box>
     </Flex>
   );
--- app/customers/[id]/CustomerButtons.tsx
+++ app/customers/[id]/CustomerButtons.tsx
@@ -1,31 +1,35 @@
 "use client";
-
 import { Flex, Button } from "@radix-ui/themes";
-import axios from "axios";
+import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 
 const CustomerButtons = ({ id }: { id: number }) => {
   const router = useRouter();
-
+  const { status } = useSession();
   return (
-    <Flex gap="3" mt="4">
-      <Button
-        variant="solid"
-        color="blue"
-        onClick={() => router.push(`/customers/${id}/edit`)}
-      >
-        Edit
-      </Button>
-      <Button
-        variant="solid"
-        color="red"
-        onClick={async () => {
-          router.push(`/customers/${id}/delete`);
-        }}
-      >
-        Delete
-      </Button>
-    </Flex>
+    <div>
+      {status === "authenticated" && (
+        <Flex gap="3" mt="4">
+          {" "}
+          <Button
+            variant="solid"
+            color="blue"
+            onClick={() => router.push(`/customers/${id}/edit`)}
+          >
+            Edit
+          </Button>
+          <Button
+            variant="solid"
+            color="red"
+            onClick={async () => {
+              router.push(`/customers/${id}/delete`);
+            }}
+          >
+            Delete
+          </Button>
+        </Flex>
+      )}
+    </div>
   );
 };
 
